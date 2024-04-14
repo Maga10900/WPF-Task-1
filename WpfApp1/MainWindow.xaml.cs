b81b5c2401b922434ac83e81813ded486806924f
Muhammad <maga168123@gmail.com>
2024-04-14 13:13:07 +0400

    Wpf Task 1 (Part 1)

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -29,7 +29,98 @@ namespace WpfApp1
         private void Button_1_Left_Click(object sender, MouseButtonEventArgs e)
         {
             Random random = new Random();
-            random.Next(1,255);
+            int R = random.Next(0,255);
+            int G = random.Next(0,255);
+            int B = random.Next(0,255);
+            Button_1.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)R,(byte)G,(byte)B));
+            MessageBox.Show(Button_1.Background.ToString());
+           
+        }
+
+        private void Button_2_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Random random = new Random();
+            int R = random.Next(0, 255);
+            int G = random.Next(0, 255);
+            int B = random.Next(0, 255);
+            Button_2.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)R, (byte)G, (byte)B));
+            MessageBox.Show(Button_2.Background.ToString());
+        }
+
+        private void Button_3_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Random random = new Random();
+            int R = random.Next(0, 255);
+            int G = random.Next(0, 255);
+            int B = random.Next(0, 255);
+            Button_3.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)R, (byte)G, (byte)B));
+            MessageBox.Show(Button_3.Background.ToString());
+        }
+
+        private void Button_4_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Random random = new Random();
+            int R = random.Next(0, 255);
+            int G = random.Next(0, 255);
+            int B = random.Next(0, 255);
+            Button_4.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)R, (byte)G, (byte)B));
+            MessageBox.Show(Button_4.Background.ToString());
+        }
+
+        private void Button_5_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Random random = new Random();
+            int R = random.Next(0, 255);
+            int G = random.Next(0, 255);
+            int B = random.Next(0, 255);
+            Button_5.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)R, (byte)G, (byte)B));
+            MessageBox.Show(Button_5.Background.ToString());
+        }
+
+        private void Button_6_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Random random = new Random();
+            int R = random.Next(0, 255);
+            int G = random.Next(0, 255);
+            int B = random.Next(0, 255);
+            Button_6.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)R, (byte)G, (byte)B));
+            MessageBox.Show(Button_6.Background.ToString());
+        }
+
+        private void Button_1_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_1.Visibility = Visibility.Hidden;
+            Title = Button_1.Content.ToString();
+        }
+
+        private void Button_2_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_2.Visibility = Visibility.Hidden;
+            Title = Button_2.Content.ToString();
+        }
+
+        private void Button_3_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_3.Visibility = Visibility.Hidden;
+            Title = Button_3.Content.ToString();
+        }
+
+        private void Button_4_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_4.Visibility = Visibility.Hidden;
+            Title = Button_4.Content.ToString();
+        }
+
+        private void Button_5_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_5.Visibility = Visibility.Hidden;
+            Title = Button_5.Content.ToString();
+        }
+
+        private void Button_6_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_6.Visibility = Visibility.Hidden;
+            Title = Button_6.Content.ToString();
         }
     }
 }
